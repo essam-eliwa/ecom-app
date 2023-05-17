932ef540f07bba4cf2a7b2222f6ec3102665265e
Essam Eliwa <essam.eliwa@miuegypt.edu.eg>
2023-05-17 21:17:25 +0300

    adjustment to Style

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('index.js: GET /');
   res.render('pages/index', { title: 'SWE230: Express Demo App' , message : 'WE LOVE MERN STACK'});
 });
 
