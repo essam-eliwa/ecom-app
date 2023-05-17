9735c2892b533e13fd2a949c54ba6de204a91659
Essam Eliwa <essam.eliwa@miuegypt.edu.eg>
2023-05-17 21:03:47 +0300

    add product_controller

---
+++ controllers/product_controller.js
@@ -0,0 +1,15 @@
+import Product from '../models/product_model.js';
+
+//get all products
+
+const getProducts = async (req, res, next) => {
+    //res.send("All Products Test Message");
+    const products = Product.find({}).then((products) => {
+      res.json(products);
+    }).catch((err) => {
+      next(err);
+    });
+  
+  };
+
+  export default getProducts;
