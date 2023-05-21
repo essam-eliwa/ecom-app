bd8ff2d6ce1bbd8f062d674450303358b4062dfe
Essam Eliwa <essam.eliwa@miuegypt.edu.eg>
2023-05-21 09:24:17 +0300

    Hashing tested

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,25 @@
 import { body, validationResult } from "express-validator";
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
 
 // matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/):
 // At least one lowercase letter ((?=.*[a-z])).
 // At least one uppercase letter ((?=.*[A-Z])).
 // At least one digit ((?=.*\d)).
-// At least one special character ((?=.*[@$!%*?&])).
-// Only allows specific characters [A-Za-z\d@$!%*?&]+.
-
+// At least one special character ((?=.*[@$!%*?&^])).
+// Only allows specific characters [A-Za-z\d@$!%*?&^]+.
+// Testing valid pass use: S2G^lPokMKau
 // Validate signup form
 const validateSignup = [
   body("username").notEmpty().withMessage("Username is required"),
   body("email").isEmail().withMessage("Invalid email"),
   body("password")
     .isLength({ min: 6 })
-    .withMessage("Password must be at least 6 characters"),
-  body("password")
+    .withMessage("Password must be at least 6 characters")
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^])[A-Za-z\d@$!%*?&^]+$/
     )
     .withMessage(
       "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character"
@@ -26,18 +29,35 @@ const validateSignup = [
     .withMessage("Passwords do not match"),
 ];
 
-// Process signup form
-const signupController = (req, res) => {
+const signupController = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.render("pages/signup", {
       title: "Signup page - Validation Failed",
       errors: errors.array(),
     });
-  } else {
-    // Process the signup logic
-    res.send("Signup successful");
+    return;
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+    const isMatch = await bcrypt.compare(
+      req.body.confirmPassword,
+      hashedPassword
+    );
+
+    if (isMatch) {
+      console.log("Passwords match");
+      res.send("Signup successful");
+    } else {
+      console.log("Passwords do not match");
+      res.send("Passwords do not match");
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("An error occurred");
   }
 };
 
 export { validateSignup, signupController };
+
